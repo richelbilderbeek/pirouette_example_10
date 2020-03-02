2da5034fcfac0226808c0d96a1da872db77aaecb
richelbilderbeek <richel@richelbilderbeek.nl>
2020-03-02 13:46:04 +0100

    Use of 'pir_save'

--- example_10.R
+++ example_10.R
@@ -6,47 +6,35 @@
 # Difference from standard: 
 # - no generative model
 #
-
 library(pirouette)
 
-################################################################################
 # Constants
-################################################################################
 is_testing <- is_on_travis()
 example_no <- 10
 rng_seed <- 314
 folder_name <- paste0("example_", example_no, "_", rng_seed)
 
-################################################################################
 # Create phylogeny
-################################################################################
 phylogeny  <- ape::read.tree(
   text = "(((A:8, B:8):1, C:9):1, ((D:8, E:8):1, F:9):1);"
 )
 
-################################################################################
 # Setup pirouette
-################################################################################
 pir_params <- create_std_pir_params(
   folder_name = folder_name
 )
 stop("Remove generative experiment here")
-
 if (is_testing) {
   pir_params <- shorten_pir_params(pir_params)
 }
 
-################################################################################
 # Run pirouette
-################################################################################
 pir_out <- pir_run(
   phylogeny,
   pir_params = pir_params
 )
 
-################################################################################
 # Save results
-################################################################################
 pir_save(
   phylogeny = phylogeny,
   pir_params = pir_params,
